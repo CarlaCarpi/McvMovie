5339655a865326ad04f1a3fb57397ed0f4343d87
CarlaCarpi <carlavc.86@gmail.com>
2025-03-30 00:20:22 -0300

    Semana 2

--- obj/Release/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Release/net8.0/MvcMovie.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba94272311864e66ae1da1fcf56961274ab63d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
--- obj/Release/net8.0/MvcMovie.RazorAssemblyInfo.cs
+++ obj/Release/net8.0/MvcMovie.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba94272311864e66ae1da1fcf56961274ab63d43")]
-[assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
